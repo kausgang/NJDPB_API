7a12a9f841ca88becff6c47dae8045a11b379854
kausgang <33816465+kausgang@users.noreply.github.com>
2025-10-22 10:27:40 -0400

    rest of the member entity

--- NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/dto/member/MemberResponseDTO.java
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/dto/member/MemberResponseDTO.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -15,4 +17,23 @@ public class MemberResponseDTO {
     private String ssn;
     private String member_num;
     private String name;
+
+
+
+    // Added By Itunuoluwa
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipcode;
+    private LocalDateTime birth_date;
+    private LocalDateTime termination_date;
+    private LocalDateTime retirement_date;
+    private LocalDateTime enrollment_Date;
+    private String veterans_flag;
+    private String calcby;
+    private Integer request_count;
+    private String chapter_flag;
+    private String former_name;
+    private String member_tier;
 }
--- NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/persistence/entity/Member.java
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/persistence/entity/Member.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "member")
 @Data
@@ -17,6 +19,24 @@ public class Member {
     private String ssn;
     private String member_num;
     private String name;
+
+
+    // Added By Itunuoluwa
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipcode;
+    private LocalDateTime birth_date;
+    private LocalDateTime termination_date;
+    private LocalDateTime retirement_date;
+    private LocalDateTime enrollment_Date;
+    private String veterans_flag;
+    private String calcby;
+    private Integer request_count;
+    private String chapter_flag;
+    private String former_name;
+    private String member_tier;
 
 
 }
