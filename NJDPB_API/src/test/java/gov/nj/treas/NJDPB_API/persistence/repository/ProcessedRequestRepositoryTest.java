611efaaa21f8b3cd2bcc88b37c3309c7b2404fd8
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2025-10-29 16:30:18 -0400

    env file

--- NJDPB_API/src/test/java/gov/nj/treas/NJDPB_API/persistence/repository/ProcessedRequestRepositoryTest.java
+++ NJDPB_API/src/test/java/gov/nj/treas/NJDPB_API/persistence/repository/ProcessedRequestRepositoryTest.java
@@ -1,59 +1,59 @@
-package gov.nj.treas.NJDPB_API.persistence.repository;
-
-import gov.nj.treas.NJDPB_API.persistence.entity.processed_request.ProcessedRequest;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
-import java.util.List;
-
-
-@DataJpaTest
-class ProcessedRequestRepositoryTest {
-
-    @Autowired
-    private TestEntityManager testEntityManager;
-
-    @Autowired
-    private ProcessedRequestRepository processedRequestRepository;
-
-
-    @BeforeEach
-    void setUp() {
-
-//        ProcessedRequestId processedRequestId = ProcessedRequestId.builder()
+//package gov.nj.treas.NJDPB_API.persistence.repository;
+//
+//import gov.nj.treas.NJDPB_API.persistence.entity.processed_request.ProcessedRequest;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//
+//import java.util.List;
+//
+//
+//@DataJpaTest
+//class ProcessedRequestRepositoryTest {
+//
+//    @Autowired
+//    private TestEntityManager testEntityManager;
+//
+//    @Autowired
+//    private ProcessedRequestRepository processedRequestRepository;
+//
+//
+//    @BeforeEach
+//    void setUp() {
+//
+////        ProcessedRequestId processedRequestId = ProcessedRequestId.builder()
+////                .ssn("123456789")
+////                .request_id(123456789)
+////                .build();
+//
+////        ProcessedRequest processedRequest = ProcessedRequest.builder()
+////                .processedRequestId(processedRequestId)
+////                .build();
+//
+//        ProcessedRequest processedRequest = ProcessedRequest.builder()
 //                .ssn("123456789")
 //                .request_id(123456789)
 //                .build();
-
-//        ProcessedRequest processedRequest = ProcessedRequest.builder()
-//                .processedRequestId(processedRequestId)
-//                .build();
-
-        ProcessedRequest processedRequest = ProcessedRequest.builder()
-                .ssn("123456789")
-                .request_id(123456789)
-                .build();
-
-        testEntityManager.persist(processedRequest);
-        testEntityManager.flush();
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void findByProcessedRequestIdSsn_ProcessedRequestFound() {
-
-//        List<ProcessedRequest> processedRequestList = processedRequestRepository.findByProcessedRequestIdSsn("123456789");
-        List<ProcessedRequest> processedRequestList = processedRequestRepository.findBySsn("123456789");
-
-        Assertions.assertThat(processedRequestList).isNotEmpty();
-
-    }
-}
+//
+//        testEntityManager.persist(processedRequest);
+//        testEntityManager.flush();
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void findByProcessedRequestIdSsn_ProcessedRequestFound() {
+//
+////        List<ProcessedRequest> processedRequestList = processedRequestRepository.findByProcessedRequestIdSsn("123456789");
+//        List<ProcessedRequest> processedRequestList = processedRequestRepository.findBySsn("123456789");
+//
+//        Assertions.assertThat(processedRequestList).isNotEmpty();
+//
+//    }
+//}
--- NJDPB_API/src/test/java/gov/nj/treas/NJDPB_API/service/impl/ProcessedRequestServiceImplTest.java
+++ NJDPB_API/src/test/java/gov/nj/treas/NJDPB_API/service/impl/ProcessedRequestServiceImplTest.java
@@ -1,96 +1,96 @@
-package gov.nj.treas.NJDPB_API.service.impl;
-
-import gov.nj.treas.NJDPB_API.dto.AggregateRequestDTO;
-import gov.nj.treas.NJDPB_API.dto.processedRequest.ProcessedRequestDTO;
-import gov.nj.treas.NJDPB_API.dto.processedRequest.ProcessedResponseDTO;
-import gov.nj.treas.NJDPB_API.mapper.ProcessedRequestMapper;
-import gov.nj.treas.NJDPB_API.persistence.entity.processed_request.ProcessedRequest;
-import gov.nj.treas.NJDPB_API.persistence.repository.ProcessedRequestRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Collections;
-import java.util.List;
-
-//import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith(MockitoExtension.class)
-class ProcessedRequestServiceImplTest {
-
-    @Mock
-    ProcessedRequestRepository processedRequestRepository;
-
-    @Mock
-    ProcessedRequestMapper processedRequestMapper;
-
-    @InjectMocks
-    ProcessedRequestServiceImpl processedRequestService;
-
-
-    private ProcessedRequestDTO processedRequestDTO;
-    private ProcessedRequest processedRequest;
-    private ProcessedResponseDTO processedResponseDTO;
-    private AggregateRequestDTO aggregateRequestDTO;
-//    private List<ProcessedResponseDTO> processedResponseDTOList;
-
-    @BeforeEach
-    void setUp() {
-
-         processedRequestDTO = ProcessedRequestDTO.builder()
-                .ssn("123456789").build();
-
-         aggregateRequestDTO = AggregateRequestDTO.builder().ssn("123456789").build();
-
-        processedRequest = ProcessedRequest.builder()
-                .ssn("123456789")
-                .request_id(12)
-                .build();
-
-        processedResponseDTO = ProcessedResponseDTO.builder()
+//package gov.nj.treas.NJDPB_API.service.impl;
+//
+//import gov.nj.treas.NJDPB_API.dto.AggregateRequestDTO;
+//import gov.nj.treas.NJDPB_API.dto.processedRequest.ProcessedRequestDTO;
+//import gov.nj.treas.NJDPB_API.dto.processedRequest.ProcessedResponseDTO;
+//import gov.nj.treas.NJDPB_API.mapper.ProcessedRequestMapper;
+//import gov.nj.treas.NJDPB_API.persistence.entity.processed_request.ProcessedRequest;
+//import gov.nj.treas.NJDPB_API.persistence.repository.ProcessedRequestRepository;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+////import static org.junit.jupiter.api.Assertions.*;
+//
+//@ExtendWith(MockitoExtension.class)
+//class ProcessedRequestServiceImplTest {
+//
+//    @Mock
+//    ProcessedRequestRepository processedRequestRepository;
+//
+//    @Mock
+//    ProcessedRequestMapper processedRequestMapper;
+//
+//    @InjectMocks
+//    ProcessedRequestServiceImpl processedRequestService;
+//
+//
+//    private ProcessedRequestDTO processedRequestDTO;
+//    private ProcessedRequest processedRequest;
+//    private ProcessedResponseDTO processedResponseDTO;
+//    private AggregateRequestDTO aggregateRequestDTO;
+////    private List<ProcessedResponseDTO> processedResponseDTOList;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//         processedRequestDTO = ProcessedRequestDTO.builder()
+//                .ssn("123456789").build();
+//
+//         aggregateRequestDTO = AggregateRequestDTO.builder().ssn("123456789").build();
+//
+//        processedRequest = ProcessedRequest.builder()
 //                .ssn("123456789")
 //                .request_id(12)
-                .build();
-
-
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getProcessedRequestBySsn() {
-
-        List<ProcessedRequest> mockProcessedRequests = List.of(processedRequest);
-        List<ProcessedResponseDTO> expectedProcessedResponseDTO = List.of(processedResponseDTO);
-
-//        when repository findBySsn is called - return a list of entity
+//                .build();
+//
+//        processedResponseDTO = ProcessedResponseDTO.builder()
+////                .ssn("123456789")
+////                .request_id(12)
+//                .build();
+//
+//
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void getProcessedRequestBySsn() {
+//
+//        List<ProcessedRequest> mockProcessedRequests = List.of(processedRequest);
+//        List<ProcessedResponseDTO> expectedProcessedResponseDTO = List.of(processedResponseDTO);
+//
+////        when repository findBySsn is called - return a list of entity
+////        Mockito.when(processedRequestRepository.findBySsn("123456789"))
+////                .thenReturn(Collections.singletonList(processedRequest));
 //        Mockito.when(processedRequestRepository.findBySsn("123456789"))
-//                .thenReturn(Collections.singletonList(processedRequest));
-        Mockito.when(processedRequestRepository.findBySsn("123456789"))
-                .thenReturn(mockProcessedRequests);
-
-
-//        when mapper.toResponseDTOlist is called, return list of responsedto
+//                .thenReturn(mockProcessedRequests);
+//
+//
+////        when mapper.toResponseDTOlist is called, return list of responsedto
+////        Mockito.when(processedRequestMapper.toResponseDTOList(Collections.singletonList(processedRequest)))
+////                .thenReturn(Collections.singletonList(processedResponseDTO));
 //        Mockito.when(processedRequestMapper.toResponseDTOList(Collections.singletonList(processedRequest)))
-//                .thenReturn(Collections.singletonList(processedResponseDTO));
-        Mockito.when(processedRequestMapper.toResponseDTOList(Collections.singletonList(processedRequest)))
-                .thenReturn(expectedProcessedResponseDTO);
-
-//        List<ProcessedResponseDTO> pr = processedRequestService.getProcessedRequestBySsn(processedRequestDTO);
-        List<ProcessedResponseDTO> pr = processedRequestService.getProcessedRequestBySsn(aggregateRequestDTO);
-
-//        Assertions.assertThat(pr).isNotEmpty();
-        Assertions.assertNotNull(pr);
-        Assertions.assertEquals(1,pr.size());
-//        Assertions.assertEquals(expectedProcessedResponseDTO.getFirst().getSsn(),pr.getFirst().getSsn());
-
-    }
-}
+//                .thenReturn(expectedProcessedResponseDTO);
+//
+////        List<ProcessedResponseDTO> pr = processedRequestService.getProcessedRequestBySsn(processedRequestDTO);
+//        List<ProcessedResponseDTO> pr = processedRequestService.getProcessedRequestBySsn(aggregateRequestDTO);
+//
+////        Assertions.assertThat(pr).isNotEmpty();
+//        Assertions.assertNotNull(pr);
+//        Assertions.assertEquals(1,pr.size());
+////        Assertions.assertEquals(expectedProcessedResponseDTO.getFirst().getSsn(),pr.getFirst().getSsn());
+//
+//    }
+//}
