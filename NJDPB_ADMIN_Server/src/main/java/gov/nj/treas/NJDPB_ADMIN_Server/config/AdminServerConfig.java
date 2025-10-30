fdfd7d96ecb49dd5c40a7d84d83c3e9884326ef5
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2025-10-29 22:24:53 -0400

    admin server

--- NJDPB_ADMIN_Server/src/main/java/gov/nj/treas/NJDPB_ADMIN_Server/config/AdminServerConfig.java
+++ NJDPB_ADMIN_Server/src/main/java/gov/nj/treas/NJDPB_ADMIN_Server/config/AdminServerConfig.java
@@ -1,7 +1,10 @@
 package gov.nj.treas.NJDPB_ADMIN_Server.config;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Configuration
 @EnableAdminServer
 public class AdminServerConfig {
 }
