600c800ccc51ed0fd5d0273cd65d6f5843ddad8e
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2025-11-03 23:42:01 -0500

    member

--- NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/controller/DummyController.java
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/controller/DummyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+@Deprecated
 @RestController
 @RequiredArgsConstructor
 public class DummyController {
---
+++ NJDPB_API/src/test/java/gov/nj/treas/NJDPB_API/service/impl/MemberServiceImplTest.java
@@ -0,0 +1,94 @@
+package gov.nj.treas.NJDPB_API.service.impl;
+
+import gov.nj.treas.NJDPB_API.dto.AggregateRequestDTO;
+import gov.nj.treas.NJDPB_API.dto.member.MemberResponseDTO;
+import gov.nj.treas.NJDPB_API.mapper.MemberMapper;
+import gov.nj.treas.NJDPB_API.persistence.entity.Member;
+import gov.nj.treas.NJDPB_API.persistence.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceImplTest {
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    MemberMapper memberMapper;
+
+    @InjectMocks
+    MemberServiceImpl memberServiceImpl;
+
+    private AggregateRequestDTO aggregateRequestDTO;
+    private Member member;
+    private MemberResponseDTO memberResponseDTO;
+
+
+    @BeforeEach
+    void setUp() {
+
+        aggregateRequestDTO = AggregateRequestDTO.builder()
+                .ssn("123456789")
+                .build();
+
+        member = Member.builder()
+                .ssn("123456789")
+                .name("Kaustav")
+                .build();
+
+        memberResponseDTO = MemberResponseDTO.builder()
+                .ssn("123456789")
+                .name("Kaustav")
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getMembersBySsn() throws ExecutionException, InterruptedException, TimeoutException {
+
+        List<Member> memberList = List.of(member);
+        List<MemberResponseDTO> expectedResponseDTOList = List.of(memberResponseDTO);
+        String ssn = aggregateRequestDTO.getSsn();
+
+        CompletableFuture<List<MemberResponseDTO>> response = memberServiceImpl.getMembersBySsn(aggregateRequestDTO);
+
+        // 1. Setup mock for Repository (Stubbing)
+        Mockito.when(memberRepository.findBySsn(ssn)).thenReturn(memberList);
+
+        // 2. Setup Mock for Mapper (Stubbing)
+        Mockito.when(memberMapper.toResponseDTOList(memberList)).thenReturn(expectedResponseDTOList);
+
+        // 3. Call the method under test
+        CompletableFuture<List<MemberResponseDTO>> futureResponse =
+                memberServiceImpl.getMembersBySsn(aggregateRequestDTO);
+
+        // 4. Extract the result from the CompletableFuture and Assert
+        List<MemberResponseDTO> actualResponseDTOList = futureResponse.get(2, TimeUnit.SECONDS);
+
+        assertEquals(expectedResponseDTOList.size(), actualResponseDTOList.size());
+        assertEquals(expectedResponseDTOList.getFirst().getSsn(), actualResponseDTOList.getFirst().getSsn());
+        assertEquals(expectedResponseDTOList.getFirst().getName(), actualResponseDTOList.getFirst().getName());
+
+
+    }
+}
