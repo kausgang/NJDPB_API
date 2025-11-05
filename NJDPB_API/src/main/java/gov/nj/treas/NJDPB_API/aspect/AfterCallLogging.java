4747fa6de2efc18a4f07920c2ff26769fe85b3f1
kausgang <33816465+kausgang@users.noreply.github.com>
2025-11-05 14:51:18 -0500

    annotation

---
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/aspect/AfterCallLogging.java
@@ -0,0 +1,22 @@
+package gov.nj.treas.NJDPB_API.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class AfterCallLogging {
+
+    @AfterReturning(pointcut = "@annotation(gov.nj.treas.NJDPB_API.annotation.DebugLogging)", returning = "result")
+    public void logResult(JoinPoint joinPoint, Object result){
+
+
+        String methodName = joinPoint.getSignature().toShortString();
+        log.debug("Repository Method {} returned: {}", methodName, result);
+        
+    }
+}
---
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/aspect/BeforeServiceLogging.java
@@ -0,0 +1,34 @@
+package gov.nj.treas.NJDPB_API.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class BeforeServiceLogging {
+
+    @Before("execution(* gov.nj.treas.NJDPB_API.service.impl.*.*(..))")
+    public void logMethodName(JoinPoint joinPoint){
+
+
+        Signature methodName = joinPoint.getSignature();
+
+        log.info("Calling {}",methodName);
+
+
+        Object[] args = joinPoint.getArgs();
+
+        log.debug("Calling method: {} With Arguments ", methodName);
+        for (int i = 0; i < args.length; i++) {
+            log.debug("Arg[{}]: {}", i, args[i]);
+        }
+
+
+    }
+}
