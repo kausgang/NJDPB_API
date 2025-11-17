bafda0be567682684920010d8060477eaf4b6322
kausgang <33816465+kausgang@users.noreply.github.com>
2025-11-17 10:57:05 -0500

    retirement init

---
+++ Retirement/src/test/java/gov/nj/treas/Retirement/RetirementApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.nj.treas.Retirement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
