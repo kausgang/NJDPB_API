3157331921804453cd1d960e7f8bac4b6928cd6e
kausgang <33816465+kausgang@users.noreply.github.com>
2025-11-18 11:37:04 -0500

    member application other column

--- NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/dto/member_application/MemberApplicationResponseDTO.java
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/dto/member_application/MemberApplicationResponseDTO.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 
 @Data
 @Builder
@@ -16,4 +18,22 @@ public class MemberApplicationResponseDTO {
 
     private Integer idn_member_num;
     private String ssnMember;
+
+
+//    private String desc_curr_operation;
+//    private String desc_type_ret;
+//    private String dte_app_received_asterisk;
+//    private String dte_cert_received_asterisk;
+//    private String dte_enrollment;
+//    private String dte_retirement;
+//    private LocalDateTime dte_Service_Format1;
+//    private LocalDateTime dte_Service_Format2;
+//    private boolean flg_Mss_Option;
+//    private String nme_entered_by;
+//    private String nme_member;
+//    private String nme_Tech;
+//    private LocalDateTime dte_service_format3;
+//    private Integer num_total_hb_serv_mos3;
+//    private Integer num_total_od_serv_mos3;
+
 }
--- NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/persistence/entity/MemberApplication.java
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/persistence/entity/MemberApplication.java
@@ -10,6 +10,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "member_application")
 @Data
@@ -23,5 +25,22 @@ public class MemberApplication {
 
     @Column(name = "ssn_member")
     private String ssnMember;
+
+//    private String desc_curr_operation;
+//    private String desc_type_ret;
+//    private String dte_app_received_asterisk;
+//    private String dte_cert_received_asterisk;
+//    private String dte_enrollment;
+//    private String dte_retirement;
+//    private LocalDateTime dte_Service_Format1;
+//    private LocalDateTime dte_Service_Format2;
+//    private Boolean flg_Mss_Option;
+//    private String nme_entered_by;
+//    private String nme_member;
+//    private String nme_Tech;
+//    private LocalDateTime dte_service_format3;
+//    private Integer num_total_hb_serv_mos3;
+//    private Integer num_total_od_serv_mos3;
+
 
 }
