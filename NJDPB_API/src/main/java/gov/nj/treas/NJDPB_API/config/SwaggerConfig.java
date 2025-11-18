f907e57671adab1089c2477017464a841e14c216
kausgang <33816465+kausgang@users.noreply.github.com>
2025-11-18 12:27:10 -0500

    security

---
+++ NJDPB_API/src/main/java/gov/nj/treas/NJDPB_API/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+
+package gov.nj.treas.NJDPB_API.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApiConfig(){
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("NJDPB Retirement API")
+                                .description("SpringBoot API to extract information from MSSql RTS database")
+                );
+    }
+}
