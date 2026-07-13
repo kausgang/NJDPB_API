fc9e2977f5c0c5832f9eeaaa494f7d72aece9681
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2026-07-13 12:33:24 -0400

    actuator permit

--- returnJWT/src/main/java/gov/njdpb/returnJWT/config/SecurityConfig.java
+++ returnJWT/src/main/java/gov/njdpb/returnJWT/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
